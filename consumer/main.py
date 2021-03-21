bb46d2f192d0565c999a2c905f83fe61fc34889a
Karl <karl.hopkinsonturrell@snyk.io>
2021-03-21 12:04:47 +0000

    first reasonable traces
    Traces are named with the correct service and sent to datadog.
    
    There is an immediately obvious inefficiency with the use of google.com
    instead of www.google.com - resulting in a 302 which increases execution
    time by about 33% for the extra request.

--- consumer/main.py
+++ consumer/main.py
@@ -1,7 +1,23 @@
+import logging
+import os
 import time
+
+from ddtrace import tracer
+import requests
+
+
+@tracer.wrap("queue.poll")
+def poll_queue():
+    requests.get("https://google.com")
+
+
+def main():
+    logging.getLogger().setLevel(logging.INFO)
+    while True:
+        poll_queue()
+        logging.info("Sleeping...")
+        time.sleep(5)
 
 
 if __name__ == "__main__":
-    while True:
-        print("Sleeping...")
-        time.sleep(5)
+    main()
