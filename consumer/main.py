80d266529813f825de647d1226a63d3112278ea7
Karl <karl.hopkinsonturrell@snyk.io>
2021-03-21 13:29:38 +0000

    add queue consuming
    The consumer now reads the queue, but generates a trace for every poll.

--- consumer/main.py
+++ consumer/main.py
@@ -2,13 +2,26 @@ import logging
 import os
 import time
 
+import boto3
 from ddtrace import tracer
 import requests
 
 
+client = boto3.client("sqs")
+
+
 @tracer.wrap("queue.poll")
 def poll_queue():
-    requests.get("https://www.google.com")
+    resp = client.receive_message(QueueUrl=os.environ.get("QUEUE_URL"))
+
+    if "Messages" in resp:
+        for message in resp["Messages"]:
+            logging.info("Received message")
+            logging.info(message)
+            client.delete_message(
+                QueueUrl=os.environ.get("QUEUE_URL"),
+                ReceiptHandle=message["ReceiptHandle"],
+            )
 
 
 def main():
