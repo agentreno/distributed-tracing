12022c4629bd5739f4e573d06062a0ccc708fbd6
Karl <karl.hopkinsonturrell@snyk.io>
2021-03-21 22:00:25 +0000

    extract context from producer
    The producer is automatically adding a trace context to the SQS message
    attributes. Read these and use this as the trace ID so that spans in the
    consumer are linked to those in the producer.
    
    This gives correct traces in datadog but the flame graphs are very
    sparse i.e. there is execution in milliseconds on each side of the queue
    and a very large gap of ~10 seconds where the messages are in the queue.

--- consumer/main.py
+++ consumer/main.py
@@ -1,9 +1,10 @@
+import json
 import logging
 import os
 import time
 
 import boto3
-from ddtrace import tracer
+from ddtrace import tracer, context
 import requests
 
 
@@ -25,7 +26,31 @@ class LoggingFilter:
         return trace
 
 
+class SQSContextExtractor:
+    def process_message(self, message):
+        # These attributes seem to be injected automatically by ddtrace on the
+        # producer side But they're not documented and they should be (but
+        # arent) being extracted automatically on the consumer side Probably
+        # ongoing work similar to
+        # https://github.com/DataDog/dd-trace-java/issues/1823
+        if "MessageAttributes" in message and "_datadog" in message["MessageAttributes"]:
+            logging.info("Found datadog trace context")
+            trace_context_data = json.loads(message["MessageAttributes"]["_datadog"]["StringValue"])
+            trace_id = int(trace_context_data.get("x-datadog-trace-id"))
+
+            # The reason I'm not using parent_id from the message is because
+            # processing this message is not part of the span in the producer.
+            # Rather it's the next span in a sequence. So I'm not setting
+            # context.span_id, we want a new span. But they're both part of the
+            # same trace still.
+            if trace_id:
+                return context.Context(trace_id=trace_id)
+
+        return None
+
+
 client = boto3.client("sqs")
+sqs_context_extractor = SQSContextExtractor()
 tracer.configure(settings={
     "FILTERS": [ProcessMessageFilter()]
 })
@@ -33,17 +58,28 @@ tracer.configure(settings={
 
 @tracer.wrap("consumer.poll")
 def poll_queue():
-    resp = client.receive_message(QueueUrl=os.environ.get("QUEUE_URL"))
+    resp = client.receive_message(
+        QueueUrl=os.environ.get("QUEUE_URL"),
+        MessageAttributeNames=["All"],
+    )
 
-    if "Messages" in resp:
+    if "Messages" not in resp:
+        return
+
+    for message in resp["Messages"]:
+        context = sqs_context_extractor.process_message(message)
+        if context:
+            logging.info("Activating context")
+            logging.info(context)
+            tracer.context_provider.activate(context)
+
         with tracer.trace("consumer.process_messages"):
-            for message in resp["Messages"]:
-                logging.info("Received message")
-                logging.info(message)
-                client.delete_message(
-                    QueueUrl=os.environ.get("QUEUE_URL"),
-                    ReceiptHandle=message["ReceiptHandle"],
-                )
+            logging.info("Received message")
+            logging.info(message)
+            client.delete_message(
+                QueueUrl=os.environ.get("QUEUE_URL"),
+                ReceiptHandle=message["ReceiptHandle"],
+            )
 
 
 def main():
