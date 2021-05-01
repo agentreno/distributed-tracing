d3d596a212bc903bf5034711db058f165450e64e
Karl <kturrell@hotmail.com>
2021-05-01 19:17:32 +0100

    move imageupdateautomation for eventapi to default ns

--- api/build.sh
+++ api/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t karlhopkinsonturrell/distributed-tracing-api .
-docker push karlhopkinsonturrell/distributed-tracing-api
+docker build -t karlhopkinsonturrell/distributed-tracing-api:${1:latest} .
+docker push karlhopkinsonturrell/distributed-tracing-api:${1:latest}
