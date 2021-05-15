25bbbd0f2ec2f9eb56396156196282ed95fa691e
Adam Gold <adamgold7@gmail.com>
2021-05-15 12:07:09 +0300

    feat: 🎸 follow trace through scrpy parts

--- producer/tracing/spiders/main.py
+++ producer/tracing/spiders/main.py
@@ -1,14 +1,27 @@
 import boto3
 import requests
 import scrapy
+from ddtrace import tracer, patch
+
+patch(botocore=True)
 
 
 class MainSpider(scrapy.Spider):
     name = "main"
-    start_urls = ["https://www.google.com"]
     ITEM_PIPELINES = {
         "tracing.pipelines.MainPipeline": 300,
     }
 
-    def parse(self, response):
-        return {"content": "test"}
+    def start_requests(self):
+        urls = [
+            "http://quotes.toscrape.com/page/1/",
+        ]
+        with tracer.start_span("producer.start_requests") as span:
+            for url in urls:
+                yield scrapy.Request(
+                    url=url, callback=self.parse, cb_kwargs={"span": span}
+                )
+
+    def parse(self, response, span):
+        with tracer.start_span("producer.parse", child_of=span):
+            return {"content": "test", "span": span}
